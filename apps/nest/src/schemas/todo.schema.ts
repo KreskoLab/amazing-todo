b5f0f1a80791558046c8e4b506822171123326a5
KreskoLab <redeezol@gmail.com>
2022-08-10 14:10:45 +0300

    feat (nest): add schema for Todo

---
+++ apps/nest/src/schemas/todo.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from 'mongoose';
+
+@Schema()
+export class Todo {
+	_id: string
+  
+  @Prop({ required: true })
+  	title: string
+
+  @Prop({ required: true, default: false })
+  	completed: boolean
+}
+
+export type TodoDocument = Todo & Document;
+
+export const TodoSchema = SchemaFactory.createForClass(Todo)
