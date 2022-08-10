7c5d3208f6f94b229166c157a1b19b285314ffaf
KreskoLab <redeezol@gmail.com>
2022-08-10 17:12:50 +0300

    feat (nest): add create method for service

--- apps/nest/src/app.service.ts
+++ apps/nest/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { Todo, TodoDocument } from './schemas/todo.schema';
 
 @Injectable()
 export class AppService {
-	getHello(): string {
-		return 'Hello World!';
+	constructor(@InjectModel(Todo.name) private todoModel: Model<TodoDocument>) {}
+
+	public async create(dto: CreateTodoDto): Promise<Todo> {
+		return this.todoModel.create(dto)
 	}
 }
