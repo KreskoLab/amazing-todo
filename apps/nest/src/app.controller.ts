83b5081498fcfe0076c5dd425dfdad8ae828d9ea
KreskoLab <redeezol@gmail.com>
2022-08-10 17:20:15 +0300

    feat (nest): add route for todo creation

--- apps/nest/src/app.controller.ts
+++ apps/nest/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { Todo } from './schemas/todo.schema';
 
-@Controller()
+@Controller('todos')
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
-	@Get()
-	getHello(): string {
-		return this.appService.getHello();
+	@Post()
+	create(@Body() dto: CreateTodoDto): Promise<Todo> {
+		return this.appService.create(dto)
 	}
 }
