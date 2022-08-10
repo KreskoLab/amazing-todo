5b3542a083a6c3a940dafb70293e7f5111838c00
KreskoLab <redeezol@gmail.com>
2022-08-10 12:31:10 +0300

    initial commit

---
+++ apps/vite/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const qwe = 'qrq';
+</script>
+
+<template>
+	<div>
+		<a
+			href="https://vitejs.dev"
+			target="_blank"
+		>
+			<img
+				src="/vite.svg"
+				class="logo"
+				alt="Vite logo"
+			/>
+		</a>
+
+		<div>qewqeqwe</div>
+	</div>
+</template>
---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "@vue/typescript/recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:vue/vue3-strongly-recommended",
+    "plugin:vue/vue3-essential",
+    "prettier",
+  ],
+  rules: {
+    "no-tabs": ["error", { "allowIndentationTabs": true }],
+		"@typescript-eslint/indent": ["error", "tab"],
+    "vue/multi-word-component-names": ["error", {
+      "ignores": []
+    }],
+    "vue/no-multi-spaces": ["error", {
+      "ignoreProperties": false
+    }],
+    "vue/singleline-html-element-content-newline": ["error", {
+      "ignoreWhenNoAttributes": true,
+      "ignoreWhenEmpty": true,
+      "ignores": ["pre", "textarea"]
+    }],
+    "vue/html-indent": ["error", "tab"]
+  }
+};
