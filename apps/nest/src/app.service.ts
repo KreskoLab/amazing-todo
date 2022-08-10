92b127eb5e64a07da0d69fe250ba883abb497aa4
KreskoLab <redeezol@gmail.com>
2022-08-10 17:15:03 +0300

    feat (nest): add findAll method for service

--- apps/nest/src/app.service.ts
+++ apps/nest/src/app.service.ts
@@ -9,11 +9,15 @@ import { Todo, TodoDocument } from './schemas/todo.schema';
 export class AppService {
 	constructor(@InjectModel(Todo.name) private todoModel: Model<TodoDocument>) {}
 
+	public async findAll(): Promise<Todo[]> {
+		return this.todoModel.find({}).select({ __v: 0 })
+	}
+
 	public async create(dto: CreateTodoDto): Promise<Todo> {
 		return this.todoModel.create(dto)
 	}
 
-	public async update(id: string, dto: UpdateTodoDto): Promise<Todo> {
+	async update(id: string, dto: UpdateTodoDto): Promise<Todo> {
 		return this.todoModel.findByIdAndUpdate(id, { ...dto }, { new: true })
 	}
 }
