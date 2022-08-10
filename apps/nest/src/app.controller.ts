61f6a2f161aa433807c9576cd31e485623f86431
KreskoLab <redeezol@gmail.com>
2022-08-10 17:22:47 +0300

    feat (nest): add route for todo update

--- apps/nest/src/app.controller.ts
+++ apps/nest/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 import { Todo } from './schemas/todo.schema';
 
 @Controller('todos')
@@ -10,5 +11,10 @@ export class AppController {
 	@Post()
 	create(@Body() dto: CreateTodoDto): Promise<Todo> {
 		return this.appService.create(dto)
+	}
+
+	@Put(':id')
+	update(@Param('id') id: string, @Body() dto: UpdateTodoDto): Promise<Todo> {
+		return this.appService.update(id, dto)
 	}
 }
