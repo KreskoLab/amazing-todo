78bfb44c47795c0c73bf293d87d1fbcc66e78766
KreskoLab <redeezol@gmail.com>
2022-08-10 13:51:05 +0300

    feat (nest): add ConfigModule

--- apps/nest/src/app.module.ts
+++ apps/nest/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: `${process.cwd()}/.env`
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
