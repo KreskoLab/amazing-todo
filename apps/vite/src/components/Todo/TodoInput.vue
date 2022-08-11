dcd46139e0ec08332fcf4a2d92814eadb2291b7d
KreskoLab <redeezol@gmail.com>
2022-08-11 16:50:47 +0300

    feat (vite): add TodoInput component

---
+++ apps/vite/src/components/Todo/TodoInput.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import TodoBase from '@/components/Todo/TodoBase.vue';
+import AppInput from '@/components/App/AppInput.vue';
+
+const emit = defineEmits<{
+	(event: 'input', text: string): void;
+}>();
+
+const todo = ref<string>('');
+
+const active = ref<boolean>(false);
+const activeVariant = computed(() => (active.value ? 'active' : 'transparent'));
+
+function addTodo() {
+	emit('input', todo.value);
+	todo.value = '';
+}
+</script>
+
+<template>
+	<TodoBase :variant="activeVariant">
+		<template #icon>
+			<svg
+				width="20"
+				height="20"
+				viewBox="0 0 20 20"
+				fill="none"
+			>
+				<path
+					d="M20 8.75H11.25V0H8.75V8.75H0V11.25H8.75V20H11.25V11.25H20V8.75Z"
+					fill="#7A7C8D"
+				/>
+			</svg>
+		</template>
+
+		<template #default>
+			<AppInput
+				v-model="todo"
+				placeholder="Add task"
+				@focus="active = true"
+				@focusout="active = false"
+				@enter="addTodo()"
+			/>
+		</template>
+	</TodoBase>
+</template>
