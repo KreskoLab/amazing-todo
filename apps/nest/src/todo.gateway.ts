7c04e7d8fb8fe456b0bcda84d096583e36e6432f
KreskoLab <redeezol@gmail.com>
2022-08-15 03:15:48 +0300

    feat (nest): add ws handlers for todo update and remove

--- apps/nest/src/todo.gateway.ts
+++ apps/nest/src/todo.gateway.ts
@@ -1,9 +1,19 @@
 import { UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
-import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WsResponse } from '@nestjs/websockets';
+import {
+	MessageBody,
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+	SubscribeMessage,
+	WebSocketGateway,
+	WsResponse,
+} from '@nestjs/websockets';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { TodoService } from './todo.service';
 import { WsExceptionFilter } from './filters/ws-exception.filter';
 import { config } from 'dotenv';
+import { ParamsDto } from './dto/params.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './schemas/todo.schema';
 
 config();
 
@@ -12,13 +22,13 @@ config();
 export class TodoGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	constructor(private readonly todoService: TodoService) {}
 
-	private readonly clients: WebSocket[] = []
+	private readonly clients: WebSocket[] = [];
 
 	handleConnection(client: WebSocket) {
-		this.clients.push(client)
+		this.clients.push(client);
 	}
 
-	 handleDisconnect(client: WebSocket) {
+	handleDisconnect(client: WebSocket) {
 		for (let i = 0; i < this.clients.length; i++) {
 			if (this.clients[i] === client) {
 				this.clients.splice(i, 1);
@@ -27,14 +37,29 @@ export class TodoGateway implements OnGatewayConnection, OnGatewayDisconnect {
 		}
 	}
 
-	private sendToClients(message: WsResponse['data']) {
-		this.clients.forEach((client) => client.send(message))
+	private sendToClients(message: WsResponse<Todo>) {
+		this.clients.forEach((client) => client.send(JSON.stringify(message)));
 	}
 
 	@UsePipes(new ValidationPipe())
 	@SubscribeMessage('add-todo')
-	async onEvent(@MessageBody() dto: CreateTodoDto): Promise<void> {
+	async addTodo(@MessageBody() dto: CreateTodoDto): Promise<void> {
 		const todo = await this.todoService.create(dto);
-		return this.sendToClients(JSON.stringify(todo))
+		return this.sendToClients({ event: 'add', data: todo });
+	}
+
+	@UsePipes(new ValidationPipe())
+	@SubscribeMessage('update-todo')
+	async updateTodo(@MessageBody() dto: UpdateTodoDto & ParamsDto): Promise<void> {
+		const { id, ...rest } = dto;
+		const todo = await this.todoService.update(id, rest);
+		return this.sendToClients({ event: 'update', data: todo });
+	}
+
+	@UsePipes(new ValidationPipe())
+	@SubscribeMessage('remove-todo')
+	async removeTodo(@MessageBody() dto: ParamsDto): Promise<void> {
+		const todo = await this.todoService.remove(dto.id);
+		return this.sendToClients({ event: 'remove', data: todo });
 	}
 }
